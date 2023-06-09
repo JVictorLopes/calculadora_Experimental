dacfd2298d6901fd8d819e71171bfe8888be3982
João Victor <jpereiralopes117@gmail.com>
2023-06-09 00:53:34 -0300

    calculadora_Experimental

---
+++ MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Alunos
+ * Data: 09/08/2022
+ * Hora: 21:59
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+namespace Calculadora_float.Parse_2._
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(181, 56);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(104, 36);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Somar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(181, 98);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(104, 36);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Subtrair";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(181, 140);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(104, 36);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Multiplicar";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(181, 182);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(104, 36);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "Dividir";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(59, 56);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(59, 98);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 5;
+			this.textBox2.TextChanged += new System.EventHandler(this.TextBox2TextChanged);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(59, 198);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 6;
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.textBox3);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.button4);
+			this.panel1.Controls.Add(this.button3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(46, 46);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(336, 314);
+			this.panel1.TabIndex = 7;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(89, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(158, 30);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "Calculadora";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(59, 182);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 15);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Resultado:";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(398, 82);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(257, 249);
+			this.pictureBox1.TabIndex = 8;
+			this.pictureBox1.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LightBlue;
+			this.ClientSize = new System.Drawing.Size(677, 422);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "Calculadora_float.Parse 2.0";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Alunos
+ * Data: 09/08/2022
+ * Hora: 21:59
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Calculadora_float.Parse_2._
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			//Soma
+			int x;
+			x=int.Parse(textBox1.Text);
+			int xx;
+			xx=int.Parse(textBox2.Text);
+			int s;
+			s=x+xx;
+			textBox3.Text=s.ToString();
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			//Subtração
+			int x;
+			x=int.Parse(textBox1.Text);
+			int xx;
+			xx=int.Parse(textBox2.Text);
+			int s;
+			s=x-xx;
+			textBox3.Text=s.ToString();
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			//Multiplicação
+			int x;
+			x=int.Parse(textBox1.Text);
+			int xx;
+			xx=int.Parse(textBox2.Text);
+			int s;
+			s=x*xx;
+			textBox3.Text=s.ToString();
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			//Float é para número fracionado. Divisão (até em frações.)
+			float x;
+			x=float.Parse(textBox1.Text);
+			float xx;
+			xx=float.Parse(textBox2.Text);
+			float s;
+			s=x/xx;
+			textBox3.Text=s.ToString();
+		}
+		void TextBox2TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
